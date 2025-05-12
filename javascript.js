ca44575dba715a22164e293968f325c8b5e1e291
Niko <197581864+Nikotheabstract@users.noreply.github.com>
2025-05-12 19:50:34 +0300

    Created a general game logic

--- javascript.js
+++ javascript.js
@@ -1,14 +1,33 @@
 function getComputerChoice(){
 let answer = Math.floor(Math.random()*3)+1;
 if (answer === 1){
-    console.log("rock");
+    console.log("computer chose rock")
+    return "rock";
 } else if (answer === 2){
-    console.log("paper");
+    console.log("computer chose paper");
+    return "paper";
 } else if (answer === 3){
-    console.log("scisors");
+    console.log("computer chose scissors");
+    return "scissors";
 }
+return answer;
 }
 
-console.log(getComputerChoice());
+function getHumanChoice(){
+   let humanChoice = prompt("rock, paper or scissors?", " ");
+   console.log(`you chose ${humanChoice}`)
+   return humanChoice;
+}  
 
+function theWinner(getComputerChoice, getHumanChoice){
+    if ((getComputerChoice === "rock" && getHumanChoice === "scissors") ||
+    (getComputerChoice === "paper" && getHumanChoice === "rock") ||
+    (getComputerChoice === "scissors" && getHumanChoice === "paper")){
+    console.log("You lost!");
+    } else if (getComputerChoice === getHumanChoice){
+        console.log("draw!");
+    } else {
+        console.log("you won!");
+    } 
+}
 
