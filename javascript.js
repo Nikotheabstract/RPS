5552fd7046e98597aada49aebdce1ad94f16b9f2
Niko <197581864+Nikotheabstract@users.noreply.github.com>
2025-05-31 19:14:56 +0300

    Added eventlistener, appended elements

--- javascript.js
+++ javascript.js
@@ -1,55 +1,63 @@
-function getComputerChoice(){
-let answer = Math.floor(Math.random()*3)+1;
-if (answer === 1){
-    console.log("computer chose rock")
-    return "rock".toLowerCase();
-} else if (answer === 2){
-    console.log("computer chose paper");
-    return "paper".toLowerCase();
-} else if (answer === 3){
-    console.log("computer chose scissors");
-    return "scissors".toLowerCase();
-}
-return answer;
-}
+let HumanChoice = ""
 
 function getHumanChoice(){
-   let humanChoice = prompt("rock, paper or scissors?", "");
-   console.log(`you chose ${humanChoice}`)
-   return humanChoice.toLowerCase();
+   HumanChoice = prompt("rock, paper or scissors?", "");
+   console.log(`you chose ${HumanChoice}`)
+   return HumanChoice.toLowerCase();
 }  
+
+const cnt = document.querySelectorAll("#container button")
+const container = document.querySelector("#container")
+let div = document.createElement("div")
+
+cnt.forEach(button =>{
+    button.addEventListener("click", () =>{
+    HumanChoice = button.id;
+    const computerChoice = getComputerChoice();
+    const result = playRound(computerChoice, HumanChoice);
+    div.textContent = `You chose ${button.id}. Computer chose ${computerChoice}. ${result}. Score: You ${humanScore} : Computer ${computerScore}`;
+    return result;
+  });
+});
+
+function getComputerChoice(){
+    let result = ""
+    let answer = Math.floor(Math.random()*3)+1;
+    if (answer === 1){
+       result = "computer chose rock"
+        return "rock".toLowerCase();
+    } else if (answer === 2){
+        result ="computer chose paper";
+        return "paper".toLowerCase();
+    } else if (answer === 3){
+       result = "computer chose scissors";
+        return "scissors".toLowerCase();
+    }
+    return result;
+    }
 
 let computerScore = 0;
 let humanScore = 0;
 
 
 function playRound(ComputerChoice, HumanChoice) {
+    let result = "";
     if ((ComputerChoice === "rock" && HumanChoice === "scissors") ||
     (ComputerChoice === "paper" && HumanChoice === "rock") ||
     (ComputerChoice === "scissors" && HumanChoice === "paper")){
         computerScore++;
         result = "you lost";
+        
     } else if (ComputerChoice === HumanChoice){
-        result = "draw"
+        result = "draw";
+        
     } else {
         humanScore++;
         result = "you won!";
     } 
-    console.log(`score: You ${humanScore} : computer ${computerScore}`);
+    div.textContent = `${result} score: You ${humanScore} : computer ${computerScore}`;
     return result
     }
 
-function playGame(){
-    playRound(getHumanChoice(), getComputerChoice());
-    playRound(getHumanChoice(), getComputerChoice());  
-    playRound(getHumanChoice(), getComputerChoice()); 
-    playRound(getHumanChoice(), getComputerChoice());  
-    playRound(getHumanChoice(), getComputerChoice()); 
-    
-    let gameResult = (humanScore > computerScore)
-        ?"you are the winner!"
-        :(humanScore < computerScore) 
-        ?"you lost!"
-        :"it's a draw!";
-    console.log(gameResult);
-}
+    container.appendChild(div);
+
